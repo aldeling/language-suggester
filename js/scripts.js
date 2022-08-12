ce50dfafec1923899acc06200cf669476b1ae170
Athea DeLing <almdeling@gmail.com>
2022-08-12 11:53:24 -0700

    add else if statements for python and swift

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 function hideSuggesterResults() {
   document.getElementById("ruby").setAttribute("class", "hidden");
+  document.getElementById("python").setAttribute("class", "hidden");
+  document.getElementById("swift").setAttribute("class", "hidden");
 }
 
 window.onload = function() {
   const form = document.querySelector("form");
   form.onsubmit = function(event){
       event.preventDefault();
+     //hideResults();
       const pet = document.querySelector("input#pet").value;
 
     if (pet === "cat") {
       document.getElementById("ruby").removeAttribute("class");
+    } else if (pet === "dog") {
+        document.getElementById("python").removeAttribute("class");
+    } else if (pet === "hamster") {
+        document.getElementById("swift").removeAttribute("class");
     }
     };
   };
