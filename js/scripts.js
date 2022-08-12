43156ed81e398243c82e5e44a658d34d8cd7a3c6
Athea DeLing <almdeling@gmail.com>
2022-08-12 10:12:30 -0700

    add radio function to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,7 @@
-//this section is for answers
-function hideLanguageResults(){
-  document.getElementById("ruby").setAttribute("class", "hidden");
-}
-
-//this section is for question
 function handleLanguage(event){
-   event.preventDefault();
- const pet = document.querySelector("input[name='pet']:checked]]").value;
-
-
+  event.preventDefault();
+  const pet=document.querySelector("input[name='pet']:checked").value;
+}
 window.addEventListener("load", function() {
   document.getElementById("questions").addEventListener("submit", handleLanguage);
-
-
-  if (pet === cat){
-    document.getElementById("ruby").removeAttribute("class");
-  }
 });
-}
