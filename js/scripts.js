2cbf7ac153f91e75a0099dfaa6ebef6130e6283a
Athea DeLing <almdeling@gmail.com>
2022-08-12 11:11:44 -0700

    change radio to user fill in, update js to reflect html

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,15 @@
-//this section is for answers
-function hideLanguageResults(){
+function hideSuggesterResults() {
   document.getElementById("ruby").setAttribute("class", "hidden");
 }
 
-//this section is for question
-function handleLanguage(event){
-   event.preventDefault();
- const pet = document.querySelector("input[name='pet']:checked]]").value;
+window.onload = function() {
+  const form = document.querySelector("form");
+  form.onsubmit = function(event){
+      event.preventDefault();
+      const pet = document.querySelector("input#pet").value;
 
-
-window.addEventListener("load", function() {
-  document.getElementById("questions").addEventListener("submit", handleLanguage);
-
-
-  if (pet === cat){
-    document.getElementById("ruby").removeAttribute("class");
-  }
-});
-}
+    if (pet === "cat") {
+      document.getElementById("ruby").removeAttribute("class");
+    }
+    };
+  };
