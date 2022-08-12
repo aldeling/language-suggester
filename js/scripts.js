5eb0e4820dd735abfe58d55f0b00e2fe5fd1e7cb
Athea DeLing <almdeling@gmail.com>
2022-08-12 10:08:14 -0700

    add js radio connect add html results, create branch to test js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
-function handleLanguage(event){
-  event.preventDefault();
-  const pet=document.querySelector("input[name='pet']:checked").value;
+//this section is for answers
+function hideLanguageResults(){
+  document.getElementById("ruby").setAttribute("class", "hidden");
 }
+
+//this section is for question
+function handleLanguage(event){
+   event.preventDefault();
+ const pet = document.querySelector("input[name='pet']:checked]]").value;
+
+
 window.addEventListener("load", function() {
   document.getElementById("questions").addEventListener("submit", handleLanguage);
+
+
+  if (pet === cat){
+    document.getElementById("ruby").removeAttribute("class");
+  }
 });
+}
